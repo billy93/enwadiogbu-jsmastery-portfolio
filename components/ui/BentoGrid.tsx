6ec36f80feb4301314fb7adc6bf192fc262f7e24
Emeka Nwadiogbu, Jr. <emekanwadiogbu.jr@me.com>
2024-08-02 19:33:58 -0700

    finished bento grid using acernity ui

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -80,7 +80,7 @@ export const BentoGridItem = ({
 
         {id === 6 && (
           <BackgroundGradientAnimation>
-            <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl" />
           </BackgroundGradientAnimation>
         )}
 
