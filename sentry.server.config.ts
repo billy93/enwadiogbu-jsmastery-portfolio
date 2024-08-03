af2edc2cedb580b6a39c6ab7d95115d0848b9e5f
Emeka Nwadiogbu, Jr. <emekanwadiogbu.jr@me.com>
2024-08-02 23:41:55 -0700

    Setting up Sentry, also posted a report a bug button site but currently only shows on the therehego.com/sentry-example-page extension of portfolio site

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://91d6e0da44ee3a918b3945fc66c9732d@o4507501171900416.ingest.us.sentry.io/4507711917654016",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
