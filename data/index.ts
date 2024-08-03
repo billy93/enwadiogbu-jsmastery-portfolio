79fdb77540e5395c4fec6644adac0f2558c651bf
Emeka Nwadiogbu, Jr. <emekanwadiogbu.jr@me.com>
2024-08-02 23:11:31 -0700

    completed the project section and updated the page navigation to dynamically call within the FloatingNav component to {navItems} so that all directories are showing

--- data/index.ts
+++ data/index.ts
@@ -72,11 +72,11 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    title: "AI 3D Brainwave to Explore",
+    des: "Explore the wonders of our AI driven ChatGPT bot with this captivating 3D simulation of the animations using GSAP and Three.js.",
     img: "/p1.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://github.com/adrianhajdin?tab=repositories",
+    link: "https://brainwave.therehego.com",
   },
   {
     id: 2,
@@ -100,38 +100,38 @@ export const projects = [
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
     img: "/p4.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://github.com/adrianhajdin/iphone",
+    link: "https://iphone.therehego.com",
   },
 ];
 
 export const testimonials = [
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Emeka was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Emeka's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Emeka is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Emeka was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Emeka's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Emeka is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Emeka was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Emeka's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Emeka is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Emeka was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Emeka's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Emeka is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Emeka was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Emeka's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Emeka is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
@@ -173,14 +173,14 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Computer Analyst Intern",
+    desc: "Assisted in the development of internal database systems using formal scripting, enhancing interactivity.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
+    title: "Mobile App Dev - Nucamp",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
